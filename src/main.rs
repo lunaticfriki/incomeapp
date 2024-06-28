540f3e8c6c1d355acf9ff33155424d8fdefd82f7
Oscar Ventura <ovenor11@proton.me>
2024-06-28 13:47:16 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui: AppWindow = AppWindow::new()?;
+    let ui_handle: slint::Weak<AppWindow> = ui.as_weak();
+
+    ui.on_divide_income({
+        move |string: slint::SharedString| {
+            let ui: AppWindow = ui_handle.unwrap();
+            let num: f64 = string.trim().parse().unwrap();
+
+            let tax:f64 = num * TAXPER;
+            let owner:f64 = num * OWNERPER;
+            let profit:f64 = num * PROFITPER;
+            let opex:f64 = num * OPEXPER;
+            let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpex: {:.2}", tax, owner, profit, opex);
+
+            ui.set_results(result.into());
+        }
+    });
+
+    ui.run()
+}
